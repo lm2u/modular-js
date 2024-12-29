5af02408e5f9e0697453870fc7bd2d2e8a37db79
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-12-29 19:55:28 +0800

    Finish spaghetti code main logic

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 const inputText = document.querySelector("input")
 function getInput() {
   let text = inputText.value
-  return text
+  if (text) {
+    return text
+  }else{
+    alert("Empty")
+  }
 }
 
 const addPersonBtn = document.getElementById('addPerson');
+const ul = document.getElementById("people")
 addPersonBtn.addEventListener("click",()=>{
   value = getInput()
-  const ul = document.getElementById("people")
   
   const itemLi = document.createElement("li")
   const itemSpan = document.createElement("span")
   const itemI = document.createElement("i")
 
-  itemSpan.textContent = value + " ";
-  itemI.textContent = "X";
+  if(value){
+    itemSpan.textContent = value + " ";
+    itemI.textContent = "X";
+  }else{
+    return
+  }
   
   ul.appendChild(itemLi)
   itemLi.appendChild(itemSpan)
   itemLi.appendChild(itemI)
   console.log(value)
+
+  inputText.value = "";
+})
+
+const wrapper = document.getElementById("peopleModule")
+wrapper.addEventListener("click",(e)=>{
+  function deleteItem() {
+    if(e.target.matches("i")) {
+      liRemove = e.target.closest("li");
+      ul.removeChild(liRemove)
+    }
+  }
+
+  deleteItem()
+  
+  
 })
 
